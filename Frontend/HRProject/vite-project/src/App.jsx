74b90f579f19e7b261a439811989deb178fd7586
Aditya Bhatt <meenakshi.bhtt@gmail.com>
2025-05-06 13:19:13 +0530

    app fix

--- Frontend/HRProject/vite-project/src/App.jsx
+++ Frontend/HRProject/vite-project/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
                 />
     
      <Route
-                path="/dashboard"
+                path="/candidates"
                 element={
                       <>
                        <Dashboard/>
